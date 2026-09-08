f671bb46598967021986359b3467a70eee687441
Muhammad Akmalluddin <makmalluddin123@gmail.com>
2026-09-08 18:38:25 +0700

    feat: add middleware in app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,18 @@
 // Import all needs 
 const express = require('express');
+const cors = require('cors');
 const routeaircraft = require('./modules/aircraft/aircraft-router.js')
 const routeship = require('./modules/ship/ship-router.js')
 
 // Express app 
 const app = express();
+
+// Middleware
+app.use(cors({
+  origin: ['http:/localhost:5173', 'http:/localhost:3000'],
+  methods: ['GET'],
+}));
+app.use(express.json());
 
 // routing basic
 app.get('/', (req, res) => {
