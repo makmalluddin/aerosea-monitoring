7e894f2b67b1a7967356836762e1922a98663aa7
Muhammad Akmalluddin <makmalluddin123@gmail.com>
2026-09-10 19:58:38 +0700

    feat: create fetch real-time logic for aircraft

--- backend/src/modules/aircraft/aircraft-live.js
+++ backend/src/modules/aircraft/aircraft-live.js
@@ -1,12 +1,41 @@
 // Import all needs
 const axios = require('axios');
-require('dotenv').config();
+const { getToken } = require('/home/makmalluddin/Projects/aerosea-monitoring/backend/src/utils/token-manager.js');
 
-const username = process.env.OPENSKY_USERNAME;
-const password = process.env.OPENSKY_PASSWORD;
+// Function to fetch data  
+const fetchData = async () => {
+  try {
+    const activeToken = await getToken();
 
-// Function get fresh token 
+    // Parameter for Indonesia area 
+    const areaIdn = {
+      lamin: -10.171,
+      lamax: 5.889,
+      lomin: 95.316,
+      lomax: 140.718,
+    };
 
+    // Fetch data 
+    const response = await axios.get("https://opensky-network.org/api/states/all", {
+      headers: {
+        "Authorization": `Bearer ${activeToken}`
+      },
+      params: areaIdn
+    });
+
+    // Aircraft data 
+    const nowAircraft = response.data;
+    console.log(nowAircraft);
+
+    return nowAircraft;
+  }
+
+  catch (error) {
+    console.log('Gagal Fetch Data: ', error.message);
+  }
+}
+
+fetchData();
 // Function cleaning data 
 // Function fetch data 
 // Start function 
