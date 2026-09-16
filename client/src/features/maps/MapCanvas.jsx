c72972494ab3dba89a8b5c2343e855e4b5ac1659
Muhammad Akmalluddin <makmalluddin123@gmail.com>
2026-09-16 16:17:12 +0700

    chore: initialize frontend base directory structure

---
+++ client/src/features/maps/MapCanvas.jsx
@@ -0,0 +1,14 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+
+function MapCanvas() {
+  const centerMap = [-2.548926, 118.014863];
+
+  return (
+    <MapContainer>
+
+    </MapContainer>
+  )
+}
+
+export default MapCanvas;
